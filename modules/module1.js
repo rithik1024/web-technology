e20697893412a9689ad946946e9f4982531e334b
rithik <rithikraj1024@gmail.com>
2024-09-15 01:27:47 +0530

    first commit to github

---
+++ modules/module1.js
@@ -0,0 +1,6 @@
+
+import obj1 from "./module2.js"
+import raja, { ar } from "./module2.js"
+//raja()
+console.log(obj1)
+console.log(ar)
---
+++ modules/module2.js
@@ -0,0 +1,19 @@
+//?default export
+function raja()
+{
+
+console.log("hello i am raja ")}
+
+// export default raja
+let obj1={
+    name:"Rani",
+    age:24,
+    address:'kochi',
+    country:'Mangolia'
+}
+export default obj1;
+
+//named export
+
+let arr=[23,45,67,89,1,23,12]
+export{arr as ar}
